a5add2141614957ca077f09190fe460ae3843bd1
Nubaid Amin <nubaid.amin@gmail.com>
2026-09-21 22:58:17 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from flask import Flask, render_template, request
+import mysql.connector
+
+app = Flask(__name__)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
@@ -33,3 +38,39 @@ def logout():
     id = ''
     msg = 'Logged out succesfully'
     return render_template('login.html',msg=msg, name=name, id=id)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        mydb = mysql.connector.connect(
+            host = 'remoteysql.com',
+            user = 'Rz8hqnlk4',
+            password = 'nd6wK03xe0',
+            database = 'Rz8hqnlk4'
+        )
+        mycursor = mydb.cursor()
+        print(username)
+        mycursor.execute('SELECT * FROM LoginDetails WHERE Name = %s AND Email_id = %s', (username, email))
+        account = mycursor.fetchone()
+        print(account)
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'^[^\d]*@[^\d]*\.\.[^\d]*$', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'(A-Za-z0-9)+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Kindly fill the details!'
+        else:
+            mycursor.execute('INSERT INTO LoginDetails VALUES (NULL, %s, %s, %s)', (username, password, email))
+            mydb.commit()
+            msg = 'You registration is successful!'
+            name = username
+            return render_template('index.html', msg=msg, name=name)
+    elif request.method == 'POST':
+        msg = 'Kindly fill the details!'
+    return render_template('registration.html', msg=msg)
